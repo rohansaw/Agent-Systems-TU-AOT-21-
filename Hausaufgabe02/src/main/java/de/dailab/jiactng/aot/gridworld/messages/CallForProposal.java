13db85e046936c99fdde87d23ecb177c3f8fc240
Niels Raddatz <raddatzniels@gmail.com>
2021-06-01 21:12:12 +0200

    change proposal msg content

--- Hausaufgabe02/src/main/java/de/dailab/jiactng/aot/gridworld/messages/CallForProposal.java
+++ Hausaufgabe02/src/main/java/de/dailab/jiactng/aot/gridworld/messages/CallForProposal.java
@@ -1,2 +1,17 @@
-package de.dailab.jiactng.aot.gridworld.messages;public class CallForProposal {
+package de.dailab.jiactng.aot.gridworld.messages;
+
+import de.dailab.jiactng.aot.gridworld.model.Order;
+
+public class CallForProposal extends GameMessage{
+    private static final long serialVersionUID = -4241990058796829378L;
+
+    public int deadline;
+    public int startTime;
+    public Order order;
+    public int bestBid;
+
+    @Override
+    public String toString() {
+        return String.format("CallForProposal(game=%d, startTime=%d, endTime=%d, bestBid=%d, orderID=%s)", gameId, startTime, deadline, bestBid, order.id);
+    }
 }
